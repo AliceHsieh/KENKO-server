b975e79974a58775adc628edd51cb733f1709cb9
Alice Hsieh <alicehsiehalice@gmail.com>
2019-02-09 21:04:13 -0800

    fix error

--- functions/sendText.js
+++ functions/sendText.js
@@ -1,36 +1,37 @@
 exports.handler = function(event, context, callback) {
-    // require('./ENV');
-    // execute some code finally
-    try{
-        const twilio = require('twilio'); 
-    } catch(e) {
-        callback(null, {
-            statusCode: 200,
-            body: e.message
-            });
-    }
+  // require('./ENV');
+  // execute some code finally
+  try {
+    const twilio = require("twilio");
 
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const authToken = process.env.TWILIO_AUTH_TOKEN; 
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = new twilio(accountSid, authToken);
 
     //Send Text
-    client.messages.create({
-        body: 'HELLO',
-        to: '+16047158724',  // Text this number
-        from: '+17786554127' // From myo number
-    }).then((message) => {
+    client.messages
+      .create({
+        body: "HELLO",
+        to: "+16047158724", // Text this number
+        from: "+17786554127" // From myo number
+      })
+      .then(message => {
         // console.log(message.body)
         callback(null, {
-            statusCode: 200,
-            body: 'message.body'
-            });
-    }).catch(e=> {
+          statusCode: 200,
+          body: "message.body"
+        });
+      })
+      .catch(e => {
         callback(null, {
-            statusCode: 200,
-            body: e.message
-            });
+          statusCode: 200,
+          body: e.message
+        });
+      });
+  } catch (e) {
+    callback(null, {
+      statusCode: 200,
+      body: e.message
     });
-
-
-}
+  }
+};
