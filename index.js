26a341d44df73fed339ff6332b35c53cacafa175
Alice Hsieh <alicehsiehalice@gmail.com>
2019-02-09 22:03:35 -0800

    update package.json to remove some dependencies

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const express = require('express'); 
-const cors = require('cors');
-const twilio = require('twilio'); 
-require('./ENV');
+// const express = require('express');
+// const cors = require('cors');
+// const twilio = require('twilio');
+// require('./ENV');
 
-//twilio requirements -- Texting API 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN; 
-const client = new twilio(accountSid, authToken);
+// //twilio requirements -- Texting API
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = new twilio(accountSid, authToken);
 
-const app = express(); //alias
+// const app = express(); //alias
 
-app.use(cors()); //Blocks browser from restricting any data
+// app.use(cors()); //Blocks browser from restricting any data
 
-//Welcome Page for the Server 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Express Server')
-})
+// //Welcome Page for the Server
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the Express Server')
+// })
 
-//Twilio 
-app.get('/send-text', (req, res) => {
-    //Welcome Message
-    res.send('Hello to the Twilio Server')
+// //Twilio
+// app.get('/send-text', (req, res) => {
+//     //Welcome Message
+//     res.send('Hello to the Twilio Server')
 
-    //_GET Variables
-    const { recipient, textmessage } = req.query;
+//     //_GET Variables
+//     const { recipient, textmessage } = req.query;
 
+//     //Send Text
+//     client.messages.create({
+//         body: textmessage,
+//         to: recipient,  // Text this number
+//         from: '+17786554127' // From myo number
+//     }).then((message) => console.log(message.body));
+// })
 
-    //Send Text
-    client.messages.create({
-        body: textmessage,
-        to: recipient,  // Text this number
-        from: '+17786554127' // From myo number
-    }).then((message) => console.log(message.body));
-})
-
-app.listen(4000, () => console.log("Running on Port 4000"))
+// app.listen(4000, () => console.log("Running on Port 4000"))
